f13f3e51405b3f18f1ebec3b6624fb58dccf0a98
Daniel Lopes <danielbrazlopes16@gmail.com>
2023-05-18 20:24:10 -0300

    feat: create home page

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,43 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+describe('Home Component', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [AppComponent]
+    });
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
 
-  it(`should have as title 'inicie-pokemon'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('inicie-pokemon');
+  it('should create the Home', () => {
+    expect(component).toBeTruthy();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('inicie-pokemon app is running!');
+  it('should have a logo', () => {
+    const logo: HTMLImageElement = fixture.nativeElement.querySelector('img#logo');
+    expect(logo).toBeTruthy();
   });
+
+  it('should have a title', () => {
+    const title: HTMLTitleElement = fixture.nativeElement.querySelector('h1#title');
+    expect(title).toBeTruthy();
+    expect(title.textContent).toEqual('Explore o mundo dos Pokémons');
+  })
+
+  it('should have a description', () => {
+    const description: HTMLSpanElement = fixture.nativeElement.querySelector('span#description');
+    expect(description).toBeTruthy();
+    expect(description.textContent).toEqual('Descubra todas as espécies de Pokémons');
+  })
+
+  it('should have a input to search pokemons', () => {
+    const inputContainer: HTMLDivElement = fixture.nativeElement.querySelector('div#search-input-container');
+    expect(inputContainer).toBeTruthy();
+  })
 });
